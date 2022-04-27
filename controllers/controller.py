cccad30ca1581bb4002c3f2618aee695e6d9ad6e
Anees Asghar <aneesasgharsyed@gmail.com>
2022-04-27 23:06:40 +0100

    Final code changes

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,58 +2,67 @@ from models.LinkedList import LinkedList
 
 
 def create_linked_list():
+    """ Initialize linked list. """
     return LinkedList()
 
 
 def register_country_at_start(country_list, country_name):
+    """ Add new country to the start of the linked list. """
     country_list.insert_at_start(country_name)
 
 
 def register_country_at_end(country_list, country_name):
+    """ Add new country to the end of the linked list. """
     country_list.insert_at_end(country_name)
 
 
 def register_country_after(country_list, new_country_name, target_country_name):
+    """ Add country in linked list after another country. """
     country_list.insert_after_item(target_country_name, new_country_name)
 
 
 def register_country_before(country_list, new_country_name, target_country_name):
-    country_list.insert_before_item(
-        target_country_name, 
-        new_country_name
-    )
+    """ Add country in linked list before another country. """
+    country_list.insert_before_item(target_country_name, new_country_name)
 
 
 def register_country_at_index(country_list, country_name, index):
+    """ Add country in linked list at the specified index. """
     country_list.insert_at_index(index, country_name)
 
 
 def get_country_count(country_list):
+    """ Return count of countries in linked list. """
     return country_list.get_count()
 
 
 def is_country_in_list(country_list, country_name):
+    """ Return True if country is in linked list, else False. """
     return country_list.search_item(country_name)
 
 
 def remove_first_country(country_list):
+    """ Remove the first country in linked list and return its name. """
     country_name = country_list.start_node.item
     country_list.delete_at_start()
     return country_name
 
 
 def remove_last_country(country_list):
+    """ Remove last country in linked list and return its name. """
     country_name = country_list.get_last_node() 
     country_list.delete_at_end()
     return country_name
 
 
 def remove_country(country_list, country_name):
-    res = country_list.search_item(country_name)
-    if res: 
-        country_list.delete_element_by_value(country_name)
-    return res
+    """ 
+        Remove the specified country from linked list. Return True if 
+        country was present in linked list, False if not. 
+    """
+    is_present = country_list.search_item(country_name)
 
-
-def register_country_at_index(country_list, country_name, index):
-    country_list.insert_at_index(index, country_name)
+    # if country is present, remove it from linked list
+    if is_present: country_list.delete_element_by_value(country_name)
+    
+    return is_present
--- views/view.py
+++ views/view.py
@@ -1,91 +1,100 @@
 from controllers import controller as ctrl
 
 def main():
+    # initialize the linked list
     country_list = ctrl.create_linked_list()
 
     while True:
         try:
+            # get user input
             usr_input = input().split(' ')
             command, args = usr_input[0].upper(), usr_input[1:]
 
-            if command == "RPI":
+            if command == "RPI": # Register country at the beginning
                 ctrl.register_country_at_start(
                     country_list, 
                     country_name=args[0]
                 )
+
+                # print updated list
                 country_list.traverse_list()
             
-            elif command == "RPF":
+            elif command == "RPF": # Register country at the end
                 ctrl.register_country_at_end(
                     country_list, 
                     country_name=args[0]
                 )
+                
+                # print updated list
                 country_list.traverse_list()
             
-            elif command == "RPDE":
+            elif command == "RPDE": # Register country after specified country
                 ctrl.register_country_after(
                     country_list, 
                     new_country_name=args[0], 
                     target_country_name=args[1]
                 )
+                
+                # print updated list
                 country_list.traverse_list()
             
-            elif command == "RPAE":
+            elif command == "RPAE": # Register country before specified country
                 ctrl.register_country_before(
                     country_list, 
                     new_country_name=args[0], 
                     target_country_name=args[1]
                 )
+                
+                # print updated list
                 country_list.traverse_list()
 
-            elif command == "RPII":
+            elif command == "RPII": # Register country at specified index
                 ctrl.register_country_at_index(
                     country_list, 
                     country_name=args[0], 
                     index=int(args[1])
                 )
-                country_list.traverse_list()
-
-            elif command == "RPI":
-                ctrl.register_country_at_index(
-                    country_list, 
-                    country_name=args[0], 
-                    index=int(args[1])
-                )
+                
+                # print updated list
                 country_list.traverse_list()
             
-            elif command == "VNE":
+            elif command == "VNE": # Get number of countries in list
                 count = ctrl.get_country_count(country_list)
                 print(f"O número de elementos são {count}.")
             
-            elif command == "VP":
+            elif command == "VP": # Check if country is in list
                 country_name = args[0]
-                res = ctrl.is_country_in_list(country_list, country_name)
-                if res:
-                    print(f"O país {country_name} encontra-se na lista.")
-                else:
-                    print(f"O país {country_name} não se encontra na lista.")
+                is_present = ctrl.is_country_in_list(
+                    country_list, 
+                    country_name
+                )
+                
+                if is_present: print(f"O país {country_name} encontra-se na lista.")
+                else: print(f"O país {country_name} não se encontra na lista.")
             
-            elif command == "EPE":
+            elif command == "EPE": # Remove first country in list
                 country_name = ctrl.remove_first_country(country_list)
-                if country_name:
-                    print(f"O país {country_name} foi eliminado da lista.")
+
+                # print the country that was removed
+                if country_name: print(f"O país {country_name} foi eliminado da lista.")
             
-            elif command == "EUE":
+            elif command == "EUE": # Remove last country in list
                 country_name = ctrl.remove_last_country(country_list)
-                if country_name:
-                    print(f"O país {country_name} foi eliminado da lista.")
+
+                # print the country that was removed
+                if country_name: print(f"O país {country_name} foi eliminado da lista.")
             
-            elif command == "EP":
+            elif command == "EP": # Remove the country specified
                 country_name = args[0]
-                res = ctrl.remove_country(country_list, country_name)
-                if res:
-                    print(f"O país {country_name} foi eliminado da lista.")
-                else:
-                    print(f"O país {country_name} não se encontra na lista.")
+                res = ctrl.remove_country(
+                    country_list, 
+                    country_name
+                )
+                
+                # print status message
+                if res: print(f"O país {country_name} foi eliminado da lista.")
+                else: print(f"O país {country_name} não se encontra na lista.")
             
-            else:
-                break
+            else: break
         
-        except EOFError:
-            break
+        except EOFError: break
